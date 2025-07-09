fdcc144467639bea39f7b48b6c93bb545a2b1c9c
gc-mahesh-kumar <maheshk@gcptrain.org>
2025-07-09 08:25:04 +0000

    Updated main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "<h1>Hello World from Google Cloud deployed in Cloud Run - Automated Deployment - Cloud Run</h1>"
+  return "<h1>Hello World from Google Cloud - Automated Deployment - Cloud Run +  Cloud Build</h1>"
 
 @app.route("/version")
 def version():
-  return "Helloworld 1.0\n"
+  return "Helloworld 2.0\n"
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',port=8080)
